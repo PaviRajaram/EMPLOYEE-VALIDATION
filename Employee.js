75d85bab15f699edd816777c2dbfc6db887d08ac
PaviRajaram <121500256+PaviRajaram@users.noreply.github.com>
2023-02-15 13:17:06 +0530

    Update Employee.js

--- Employee.js
+++ Employee.js
@@ -6,19 +6,31 @@ deletefn=()=>{
     document.getElementById("salary").value="";
     document.getElementById("dob").value="";   
  }
- submitfn=()=>{
-    const Ename= document.getElementById("Ename").value;
-    const Eid=document.getElementById("id").value;
+submitfn=()=>{
     const date=new Date();
-    var Employee={
-        id:Eid,
-        pswd:date.getTime()
-    };
- var employee=JSON.stringify(Employee);
- console.log(Employees);
- Employees.push(employee);
- localStorage.Employee+=Employees;
- localStorage.setItem("Employee",Employees);
- //localStorage.Employees+=JSON.stringify(Employee);
- alert(employee);
+    
+ //localStorage.Employee+=Employees;
+ //localStorage.setItem("Employee",JSON.stringify(Employees));
+
+    let name=document.getElementById("Ename").value;
+    let desgn=document.getElementById("dsgn").value;
+    let  Eid=document.getElementById("id").value;
+    let salary= document.getElementById("salary").value;
+    var numbers = /^[0-9]+$/;
+    var letters = /^[A-Za-z]+$/;
+    if(salary.match(numbers)&&name.match(letters)&&desgn.match(letters)&&Eid.match(numbers)){
+        var Employee={
+            id:Eid,
+            //pswd:date.getTime()
+            pswd:Eid+1
+        };
+        var employee=JSON.stringify(Employee);
+        console.log(employee);
+        Employees.push(employee);
+        localStorage.setItem("Employee",Employees);
+        alert(employee);
+       }
+       else{
+        alert("Enter valid input");
+     }
  }
