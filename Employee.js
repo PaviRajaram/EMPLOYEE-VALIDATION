d9f7116067d5fcecd5d3a90cc251a8d1d4d1a8a3
PaviRajaram <121500256+PaviRajaram@users.noreply.github.com>
2023-02-24 10:54:33 +0530

    Update Employee.js

--- Employee.js
+++ Employee.js
@@ -20,15 +20,30 @@ submitfn=()=>{
             //pswd:date.getTime()
             pswd:Eid+1
         };
-        var employee=JSON.stringify(Employee);
-        console.log(employee);
-        Employees.push(employee);
-        localStorage.setItem("Employee",Employees);
-     //localStorage.Employee+=Employees;
-     //localStorage.setItem("Employee",JSON.stringify(Employees));
-        alert(employee);
-       }
-       else{
-        alert("Enter valid input");
-     }
+        console.log(Employee);
+        if(localStorage.getItem("Employee")==null){
+        Employees.push(Employee);
+        localStorage.setItem("Employee",JSON.stringify(Employees));
+        alert(JSON.stringify(Employee));
+        }
+        else{
+        var idP=JSON.parse(localStorage.getItem('Employee'));
+        console.log("getmethod",idP);
+        for(let i=idP.length-1;i>=0;i--){
+        var user=idP.filter((employee)=>{return employee.id==Employee.id})[i];
+        }
+        console.log("user :",user);
+            if(user)
+            {
+            alert("Id already exists..");
+            }
+            else{
+            Employees.push(Employee);
+            localStorage.setItem("Employee",JSON.stringify(Employees));
+            alert(JSON.stringify(Employee));
+        }
+    }
+ }else{
+    alert("Invalid data..")
  }
+}
